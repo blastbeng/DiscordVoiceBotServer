628b4018c3aaa96ab65efe1f1bc0e27119fc4831
blast <rodfval.fv@gmail.com>
2022-05-04 15:55:45 +0000

    aaa

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,6 @@ import logging
 import utils
 import insults
 import tournament
-import trivia
 from flask import Flask, request, send_file, Response, jsonify
 from flask_restx import Api, Resource, reqparse
 from chatterbot.conversation import Statement
@@ -13,7 +12,6 @@ log.setLevel(logging.ERROR)
 
 chatbot = utils.get_chatterbot(None, False)
 tournament.create_empty_tables()
-trivia.create_empty_tables()
 
 app = Flask(__name__)
 api = Api(app)
@@ -148,28 +146,6 @@ class YoutubeSearchClass(Resource):
     text = request.args.get("text")
     onevideo = request.args.get("onevideo")
     return utils.search_youtube_audio(text, bool(onevideo))
-
-parsertriviaget = reqparse.RequestParser()
-parsertriviaget.add_argument("author")
-parsertriviaget.add_argument("amount")
-parsertriviaget.add_argument("category")
-parsertriviaget.add_argument("difficulty")
-parsertriviaget.add_argument("type")
-
-nstrivia = api.namespace('chatbot_trivia', 'Accumulators Chatbot Trivia APIs')
-@nstrivia.route('/getquiz')
-class TriviaGet(Resource):
-  @api.expect(parsertriviaget)
-  def get(self):
-    author = request.args.get("author")
-    amount = request.args.get("amount")
-    category = request.args.get("category")
-    difficulty = request.args.get("difficulty")
-    typeq = request.args.get("type")
-    return trivia.get_quiz(author, amount, category, difficulty, typeq)
-
-
-
 
 if __name__ == '__main__':
   app.run()
