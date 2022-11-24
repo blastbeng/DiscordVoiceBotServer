004cbabc6ff73bd10da3d4b03492d8b0be3e5938
blast <rodfval.fv@gmail.com>
2022-11-24 17:11:07 +0100

    aaa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p /app/config/backups/
 cp /app/config/*db.sqlite3 /app/config/backups/
-uwsgi --ini uwsgi.ini --enable-threads
+uwsgi --ini /app/config/uwsgi.ini --enable-threads
